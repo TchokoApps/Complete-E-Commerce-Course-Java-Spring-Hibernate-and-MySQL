501f6048cfceee84f1ee3632ce4a88a0965169f0
TchokoApps <tchokoapps@gmail.com>
2023-02-28 19:43:54 +0100

    Step 007

--- ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/backend/controller/UserController.java
+++ ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/backend/controller/UserController.java
@@ -4,6 +4,7 @@ import com.tchokoapps.springboot.ecommerce.backend.entity.User;
 import com.tchokoapps.springboot.ecommerce.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -18,7 +19,9 @@ public class UserController {
     }
 
     @GetMapping("backend/users")
-    List<User> findAll() {
-        return userService.findAll();
+    String findAll(Model model) {
+        final List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "backend/users";
     }
 }
---
+++ ecommerce/src/main/resources/data.sql
@@ -0,0 +1,40 @@
+INSERT INTO roles (name, description)
+VALUES ('ADMIN', 'Can perform all actions');
+INSERT INTO roles (name, description)
+VALUES ('USER', 'Can view and edit own data');
+INSERT INTO roles (name, description)
+VALUES ('MANAGER', 'Can manage resources and team');
+INSERT INTO roles (name, description)
+VALUES ('GUEST', 'Can view limited content');
+INSERT INTO roles (name, description)
+VALUES ('DEVELOPER', 'Can develop and deploy applications');
+INSERT INTO roles (name, description)
+VALUES ('TESTER', 'Can test applications and report issues');
+INSERT INTO roles (name, description)
+VALUES ('ANALYST', 'Can analyze data and provide insights');
+INSERT INTO roles (name, description)
+VALUES ('DESIGNER', 'Can design user interfaces and experiences');
+INSERT INTO roles (name, description)
+VALUES ('CONTENT_WRITER', 'Can write and publish content');
+INSERT INTO roles (name, description)
+VALUES ('MARKETER', 'Can market products and services');
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('john.doe@gmail.com', 'password123', 'John', 'Doe', 'john-doe.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('jane.doe@gmail.com', 'password123', 'Jane', 'Doe', 'jane-doe.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('jack.smith@gmail.com', 'password123', 'Jack', 'Smith', 'jack-smith.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('jill.smith@gmail.com', 'password123', 'Jill', 'Smith', 'jill-smith.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('mark.johnson@gmail.com', 'password123', 'Mark', 'Johnson', 'mark-johnson.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('sara.johnson@gmail.com', 'password123', 'Sara', 'Johnson', 'sara-johnson.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('james.wilson@gmail.com', 'password123', 'James', 'Wilson', 'james-wilson.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('emily.wilson@gmail.com', 'password123', 'Emily', 'Wilson', 'emily-wilson.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('david.brown@gmail.com', 'password123', 'David', 'Brown', 'david-brown.jpg', true);
+INSERT INTO users (email, password, first_name, last_name, photos, enabled)
+VALUES ('lisa.brown@gmail.com', 'password123', 'Lisa', 'Brown', 'lisa-brown.jpg', true);
