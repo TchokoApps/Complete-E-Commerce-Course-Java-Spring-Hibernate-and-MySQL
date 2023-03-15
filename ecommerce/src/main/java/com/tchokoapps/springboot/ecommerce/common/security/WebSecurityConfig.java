0b0e7456c4cd2d59624ebac190da787716152394
TchokoApps <tchokoapps@gmail.com>
2023-03-15 20:11:48 +0100

    Step 034 Show login page

--- ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/common/security/WebSecurityConfig.java
+++ ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/common/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.tchokoapps.springboot.ecommerce.common.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,11 +20,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .antMatchers("/").permitAll()
-//                .antMatchers("/h2-console/**").permitAll();
-        http.authorizeRequests().anyRequest().permitAll();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/admin/login")
+                .usernameParameter("email")
+                .permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/backend/**");
     }
 }
