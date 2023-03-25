da2395f686e0bd379c375d8f6b3c675b52e0071d
Tientcheu Tchokouaha <tchokoapps@gmail.com>
2023-03-26 00:17:54 +0100

    step 048 Save created Category and handle exceptions

--- ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/backend/controller/CategoryController.java
+++ ecommerce/src/main/java/com/tchokoapps/springboot/ecommerce/backend/controller/CategoryController.java
@@ -3,13 +3,20 @@ package com.tchokoapps.springboot.ecommerce.backend.controller;
 import com.tchokoapps.springboot.ecommerce.backend.entity.Category;
 import com.tchokoapps.springboot.ecommerce.backend.service.CategoryService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class CategoryController {
@@ -36,5 +43,25 @@ public class CategoryController {
         model.addAttribute("categories", categories);
         addMessage(redirectAttributes, "Category Created Successfully", "success");
         return "admin/categories/create-form";
+    }
+
+    @PostMapping("admin/categories/create")
+    public String createCategory(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model, @RequestParam(name = "image") MultipartFile multipartFile) {
+        log.info("createCategory() :: create Category {}", category);
+
+        if (bindingResult.hasErrors()) {
+            List<Category> categories = categoryService.findAllHierarchically();
+            model.addAttribute("categories", categories);
+            return "admin/categories/create-form";
+        }
+
+        try {
+            categoryService.save(category);
+            addMessage(redirectAttributes, "Category Created Successfully", "success");
+        } catch (Exception e) {
+            addMessage(redirectAttributes, e.getMessage(), "error");
+        }
+        return "redirect:/admin/categories";
+
     }
 }
